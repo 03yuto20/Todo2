2bfaa79cce631c7bbbfc6178f8e42b2c75d25697
Yuto Nakamura <yutonakamura0320@gmail.com>
2018-08-12 11:29:50 +0900

    Initial Commit

---
+++ Todo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo2
+//
+//  Created by Yuto Nakamura on 2018/08/12.
+//  Copyright © 2018年 Yuto Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
